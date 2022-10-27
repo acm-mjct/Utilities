92202e041eb754a1e99cac7618ad0a51f5690d07
TAHINIYATH <160420733064@mjcollege.ac.in>
2022-10-27 15:28:42 +0530

    adding two numbers using functions

---
+++ lib/c/addition.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int add(int a,int b)
+{
+  int a,b;
+  float c;
+  c=a+b;
+}
+int main(){
+    int a,b;
+    printf("enter 2 numbers\n");
+    scanf("%d %d",&a,&b);
+    add(a,b);
+}
