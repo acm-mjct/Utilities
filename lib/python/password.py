29946edf4620d9aca658f3a23e83d08b61d0704b
rayyan21d <69739582+RayyanUddin@users.noreply.github.com>
2022-10-31 07:28:45 +0530

    Js hello (#251)
    Co-authored-by: Parbez <imranbarbhuiya.fsd@gmail.com>

--- lib/python/password.py
+++ lib/python/password.py
@@ -6,7 +6,7 @@ Alphabet = Alphabet + Alphabet.upper()
 a=""
 print("I'm gonna generate a sequence of characters")
 
-for i in range(100):
+for i in range(111):
 
     a=a+(string[i%13])
     a=a+(numbers[(i+3)%7])
